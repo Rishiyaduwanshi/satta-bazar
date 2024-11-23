15d2ad8096a686e94bee85b11621d3948052b9d4
Abhinav Prakash <abprakash85409@gmail.com>
2024-11-23 18:10:02 +0530

    Fix -> Show results only for games in the predefined game list

--- handlers/manageResults.handler.js
+++ handlers/manageResults.handler.js
@@ -1,21 +1,20 @@
 const Result = require("../models/resultSchema");
+const games = require('../data/gameList');
 
 const manageResults = async (req, res) => {
   try {
     const { game, date } = req.query;
-    let query = {};
+    let query = {'game' : {$in : games}};
 
     if (game) query.game = game;
-    if (date) {
+    if (date && !isNaN(new Date(date))) {
       const startDate = new Date(date);
       const endDate = new Date(date);
       endDate.setDate(endDate.getDate() + 1);
       query.date = { $gte: startDate, $lt: endDate };
     }
 
-    const games = await Result.distinct("game");
     const results = await Result.find(query).sort({ date: -1 }).limit(100);
-
     res.render("manageResults", {
       games,
       results,
